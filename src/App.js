d8b9a73b60b5a2143434349f63ae4816355d6b67
Henry Keen <henrykeen@users.noreply.github.com>
2021-02-25 11:30:00 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div>
+        <div>
+          <label>
+            Hero
+            <input></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Villain
+            <input></input>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders the hero and villain labels', () => {
+  render(<App />);
+  const heroLabel = screen.getByText(/Hero/i);
+  const villainLabel = screen.getByText(/Villain/i);
+
+  expect(heroLabel).toBeInTheDocument();
+  expect(villainLabel).toBeInTheDocument();
+});

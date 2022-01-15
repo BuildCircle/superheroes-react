4963f9b4c631f88dbbfffacb9352141ae1f44936
Alex Meah <alexmeahdotcom@gmail.com>
2022-01-15 16:58:36 +0000

    Add Tailwind and related deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="font-sans">
+      <header>
         <div>
           <div>
             <label>
